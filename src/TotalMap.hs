c8382dff65c9a7ddba531ff66203d36feef2c8fd
Eric Mertens <emertens@gmail.com>
2018-09-22 13:40:55 -0700

    TotalMap haddocks

--- src/TotalMap.hs
+++ src/TotalMap.hs
@@ -51,8 +51,10 @@ data TotalMap k v = TotalMap v !(Map k v)
 --
 -- >>> fromList 0 [('a',1), ('b',2)]
 -- fromList 0 [('a',1),('b',2)]
+--
 -- >>> fromList 0 [('a',1), ('a',2)]
 -- fromList 0 [('a',2)]
+--
 -- >>> fromList 0 [('a',0)]
 -- fromList 0 [('a',0)]
 fromList ::
@@ -66,8 +68,10 @@ fromList def xs = TotalMap def (Map.fromList xs)
 -- | Lookup the value associated with a given key in the map.
 --
 -- >>> let m = fromList 0 [('a',1), ('b',2)]
+--
 -- >>> m ! 'b'
 -- 2
+--
 -- >>> m ! 'c'
 -- 0
 (!) :: Ord k => TotalMap k v -> k -> v
@@ -76,11 +80,19 @@ TotalMap def m ! key = Map.findWithDefault def key m
 
 -- | Assign a new value to a given key in a map.
 --
+-- Note that this assignment makes no attempt to avoid duplicating
+-- the default value.
+--
 -- >>> let m = fromList 0 [('a',1), ('b',2)]
--- >>> ('b' =: 10) m ! 'b'
--- 10
--- >>> ('c' =: 10) m ! 'b'
--- 2
+--
+-- >>> ('b' =: 0) m
+-- fromList 0 [('a',1),('b',0)]
+--
+-- >>> ('b' =: 10) m
+-- fromList 0 [('a',1),('b',10)]
+--
+-- >>> ('c' =: 10) m
+-- fromList 0 [('a',1),('b',2),('c',10)]
 (=:) :: Ord k => k -> v -> TotalMap k v -> TotalMap k v
 (k =: v) (TotalMap def m) = TotalMap def (Map.insert k v m)
 
@@ -94,19 +106,28 @@ instance (Ord k, Choice v) => Choice (TotalMap k v) where
 
 -- | This instance allows you to apply a function to the values
 -- stored in the total map.
+--
+-- >>> fmap (*2) (fromList 10 [('a',20)])
+-- fromList 20 [('a',40)]
 instance Functor (TotalMap k) where
   fmap f (TotalMap def m) = TotalMap (f def) (fmap f m)
 
 -- | This instance provides point-wise application between
 -- two total maps. The meaning of 'pure' is to provide a map
 -- that always returns the given value.
+--
+-- >>> pure 1 :: TotalMap Char Int
+-- fromList 1 []
+--
+-- >>> fromList id [('a',(+1)),('b',subtract 1)] <*> fromList 4 [('b',5), ('c',6)]
+-- fromList 4 [('a',5),('b',4),('c',6)]
 instance Ord k => Applicative (TotalMap k) where
   pure def = TotalMap def Map.empty
   TotalMap fDef fMap <*> TotalMap xDef xMap =
     TotalMap
-      (fDef xDef)
+      (fDef xDef) -- combine default function and argument
       (Map.mergeWithKey
-         (\_key f x -> Just (f x))
+         (\_key f x -> Just (f x)) -- combine specific function and argument
          (fmap (\f -> f xDef)) -- use default argument
          (fmap (\x -> fDef x)) -- use default function
          fMap xMap)
